a264bfc75d139400be0ed73decc2af98edf8785b
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-07 16:40:14 +0200

    prime due colonne footer

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,3 +1,56 @@
 <footer>
+  <div class="container">
+      <div class="list-info">
+      {{-- Column 1--}}
+        <div class="list-column">
+            <div class="logo">
+                <img src="{{ asset('images/marchio-sito-test.png')}}" alt="Logo">
+            </div>
+            <ul>
+              <li>Ragione Sociale: La Molisana SPA</li>
+              <li>Sede Legale: Contrada Colle delle Api, 100/A - 86100 - Campobasso (CB)</li>
+              <li>Pec: lamolisana@pec.it</li>
+              <li>Tel: +39 0874 4981</li>
+              <li>Fax: +39 0874 629584</li>
+              <li>info@lamolisana.it</li>
+              <li>commerciale@lamolisana.it</li>
+              <li>export@lamolisana.it</li>
+              <li>numero verde: 800 987 543</li>
+              <li>telefono: 700 111 222</li>
+            </ul>
+        </div>
+     {{-- /Column 1--}}
+      {{-- Column 2--}}
+        <div class="list-column">
+        {{-- Pastificio --}}
+            <h3>Pastificio</h3>
+            <ul>
+              <li>Il Pastificio</li>
+              <li>Grano decorticato a pietra</li>
+              <li>Il Molise c’è</li>
+              <li>Filiera Integrata</li>
+              <li>100 anni di pasta</li>
+              <li>Sartoria della pasta</li>
+              <li>Spaghetto Quadrato</li>
+              <li>Le Persone</li>
+            </ul>
+        {{-- /Pastificio --}}
+        <h3>Prodotti</h3>
+         <ul>
+           <li>Le Classiche</li>
+           <li>Le Integrali</li>
+           <li>Le Speciali</li>
+           <li>Le Biologiche</li>
+           <li>Le Gluten Free</li>
+           <li>Le Semole</li>
+           <li>Le Extra-Lusso</li>
+         </ul>
+
+        </div>
+     {{-- /Column 2--}}
+     {{-- Column 3--}}
+     {{-- /Column 3--}}
+      </div>
+  </div>
 
 </footer>
