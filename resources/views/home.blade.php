0bdcf97f1993a76edaaadca7acd0f04ee6bf178e
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-07 15:43:54 +0200

    stampo la sezione paste lunghe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,27 @@
 <html lang="en">
 @include('parts/head')
 <body>
+{{-- Header Incluso --}}
  @include('parts.header')
+{{-- /Header Incluso --}}
     <main>
+      <div class="container">
+        <section>
+          <h2>Le Lunghe</h2>
+          <div class="container-boxes">
+              @foreach ($lunghe as $pasta)
+              <div class="box">
+                <img src="{{ $pasta['src']}}" alt="{{ $pasta['titolo']}}">
+              </div>
+              @endforeach
+          </div>
+        </section>
+      </div>
 
     </main>
+
+{{-- Footer Incluso --}}
 @include('parts/footer')
+{{-- /Footer Incluso --}}
 </body>
 </html>
